dd60c49286026a3cac93a68ce0d37040a106944b
adriana-rv <adriana.rv@gmail.com>
2015-08-17 16:52:04 -0500

    Add re permissions for validators

--- src/nudos/theme/__init__.py
+++ src/nudos/theme/__init__.py
@@ -4,3 +4,27 @@
 from zope.i18nmessageid import MessageFactory
 
 _ = MessageFactory('nudos.theme')
+
+
+# Products/PythonScripts/module_access_examples.py
+from AccessControl import allow_type
+from AccessControl import ModuleSecurityInfo
+
+ModuleSecurityInfo('re').declarePublic(
+    'compile',
+    'findall',
+    'match',
+    'search',
+    'split',
+    'sub',
+    'subn',
+    'error',
+    'I',
+    'L',
+    'M',
+    'S',
+    'X',
+)
+import re
+allow_type(type(re.compile('')))
+allow_type(type(re.match('x','x')))
